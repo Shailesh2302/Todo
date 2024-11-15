0e0186f5e3a3eeeecf5d6b30a283dda4a074c665
Shailesh_18_ <rushikeshkanfade123@gmail.com>
2024-11-15 16:57:29 +0530

    added delete, update, complete operations

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,29 @@ function App() {
   const [todos, setTodos] = useState([]);
 
   const addTodo = (todo) => {
-setTodos((prev) => [{id:Date.now(), ...todo},...prev])
-  }
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
+  };
 
-  const updatedTodo = (id,todo) => {
+  const updatedTodo = (id, todo) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
+    );
 
+    const deleteTodo = (id) => {
+      setTodos((prev) => prev.filter((todo) => todo.id !== id));
+    };
+  };
 
-  }
+  const toggleComplete = (id) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) =>
+        prevTodo === id
+          ? { ...prevTodo, 
+            completed: !prevTodo.completed }
+          : prevTodo
+      )
+    );
+  };
   return (
     <TodoProvider
       value={{ todos, addTodo, updatedTodo, deleteTodo, toggleComplete }}
